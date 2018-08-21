a67a93cb37ea87830d4184b137a781109dc82a1e
SenCaoKing <1134349927@qq.com>
2018-08-21 22:54:16 +0800

    Auth之用户组管理

--- Application/Common/Model/UsersModel.class.php
+++ Application/Common/Model/UsersModel.class.php
@@ -1,11 +1,51 @@
 <?php
 namespace Common\Model;
 use Common\Model\BaseModel;
-
 /**
- * Class BaseModel
- * @package Common\Model
+ * ModelName
  */
-class UsersModel extends Model {
+class UsersModel extends BaseModel {
+    // 自动验证
+    protected $_validate=array(
+        array('username', 'require', '用户名必须', 0, '', 3), // 验证字段必填
+    );
+
+    // 自动完成
+    protected $_auto=array(
+        array('password', 'md5', 1, 'function'), // 对password字段在新增的时候使md5函数处理
+        array('register_time', 'time', 1, 'function'), // 对register_time字段在新增的时候写入当前时间戳
+    );
+
+    /**
+     * 添加用户
+     */
+    public function addData($data){
+        // 对data数据进行验证
+        if(!$data=$this->create($data)){
+            // 验证不通过返回错误
+            return false;
+        }else{
+            // 验证通过
+            $result=$this->add($data);
+            return $result;
+        }
+    }
+
+    /**
+     * 修改用户
+     */
+    public function editData($map, $data){
+        // 对data数据进行验证
+        if(!$data=$this->create($data)){
+            // 验证不通过返回错误
+            return false;
+        }else{
+            // 验证通过
+            $result=$this
+                ->where(array($map))
+                ->save($data);
+            return $result;
+        }
+    }
 
 }

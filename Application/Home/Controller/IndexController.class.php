4cbb7505cf2afdfb86b0619d161c85b19b18a230
SenCaoKing <1134349927@qq.com>
2018-08-28 21:40:46 +0800

    thinkphp系列之webuploader异步预览上传

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -102,6 +102,29 @@ class IndexController extends HomeBaseController{
         echo intval(geetest_check_verify($data));
     }
 
+    /**
+     * webuploader 上传文件
+     */
+    public function ajax_upload(){
+        // 根据自己的业务调整上传路径、允许的格式、文件大小
+        ajax_upload('Upload/image/');
+    }
+
+    /**
+     * webuploader 上传demo
+     */
+    public function webuploader(){
+        // 如果是post提交则显示上传的文件 否则显示上传页面
+        if(IS_POST){
+            dump($_POST);die;
+        }else{
+            $this->display();
+        }
+    }
+
+
+
+
     // 首页分页测试
     public function index(){
         $count = M('Articles')->count();
